437b343ef4bd0340c6cf436c57553ee8d094c65e
Punit Nakum <125398856+Punit1401@users.noreply.github.com>
2026-02-13 21:39:45 +0530

    Update program to find largest number using if-else

--- p6.py
+++ p6.py
@@ -1,1 +1,17 @@
-# Write a program to print the largest number between 3 numbers using nested loop.
+# Write a program to print the largest number between 3 numbers using nested if-else.
+
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+num3 = int(input("Enter number3: "))
+
+if(num1>num2):
+    if(num1>num3):
+        print("Biggest number is: ",num1)
+    else:
+        print("Biggest number is: ",num3)
+else:
+    if(num2>num3):
+        print("Biggest number is:",num2)
+    else:
+        print("Biggest number is:",num3)
+    
